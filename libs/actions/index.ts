2a2163c259bfa8fa26a42522902d8b48d886eda0
omsant02 <omsantoshwar02@gmail.com>
2024-02-16 23:54:45 +0530

    connect mongodb

--- libs/actions/index.ts
+++ libs/actions/index.ts
@@ -1,11 +1,19 @@
 "use server";
 
+// import { revalidatePath } from "next/cache";
+// import Product from "../models/product.model";
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
+// import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
 
   try {
+    connectToDB();
+
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
     if (!scrapedProduct) return;
