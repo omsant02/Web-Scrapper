a2ab44973b474519fb2111b63533f6c27978d3e8
omsant02 <omsantoshwar02@gmail.com>
2024-02-16 21:02:20 +0530

    scrap data - price

--- libs/scraper/index.ts
+++ libs/scraper/index.ts
@@ -1,3 +1,41 @@
 "use server";
 
-export async function scrapeAmazonProduct(url: string) {}
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
+
+export async function scrapeAmazonProduct(url: string) {
+  if (!url) return;
+  // BrightData proxy configuration
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
+
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: "brd.superproxy.io",
+    port,
+    rejectUnauthorized: false,
+  };
+
+  try {
+    // Fetch the product page
+    const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
+
+    // Extract the product title
+    const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+    console.log(title, currentPrice);
+  } catch (error: any) {
+    console.log(error);
+  }
+}
---
+++ libs/utils.ts
@@ -0,0 +1,31 @@
+// import { PriceHistoryItem, Product } from "@/types";
+
+const Notification = {
+  WELCOME: "WELCOME",
+  CHANGE_OF_STOCK: "CHANGE_OF_STOCK",
+  LOWEST_PRICE: "LOWEST_PRICE",
+  THRESHOLD_MET: "THRESHOLD_MET",
+};
+
+const THRESHOLD_PERCENTAGE = 40;
+
+// Extracts and returns the price from a list of possible elements.
+export function extractPrice(...elements: any) {
+  for (const element of elements) {
+    const priceText = element.text().trim();
+
+    if (priceText) {
+      const cleanPrice = priceText.replace(/[^\d.]/g, "");
+
+      let firstPrice;
+
+      if (cleanPrice) {
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+      }
+
+      return firstPrice || cleanPrice;
+    }
+  }
+
+  return "";
+}
