d93bb0d10e6b17acb0e32439694db76008772f54
omsant02 <omsantoshwar02@gmail.com>
2024-02-17 13:23:32 +0530

    imp

--- libs/actions/index.ts
+++ libs/actions/index.ts
@@ -5,7 +5,7 @@ import Product from "../../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
-import { User } from "@/types";
+import { User } from "../../types";
 import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
