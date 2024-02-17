a6576cfb34e81d3111f56cc37a522a7433f8e27a
omsant02 <omsantoshwar02@gmail.com>
2024-02-17 16:38:35 +0530

    add cron route

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ const Home = async () => {
       <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
-            {" "}
             <p className="small-text">
               Smart Shopping Starts Here:
               <Image
@@ -22,19 +21,24 @@ const Home = async () => {
                 height={16}
               />
             </p>
+
             <h1 className="head-text">
               Unleash the Power of
               <span className="text-primary"> PriceWise</span>
             </h1>
+
             <p className="mt-6">
               Powerful, self-serve product and growth analytics to help you
               convert, engage, and retain more.
             </p>
+
             <Searchbar />
           </div>
+
           <HeroCarousel />
         </div>
       </section>
+
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
 
--- libs/actions/index.ts
+++ libs/actions/index.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import Product from "../../models/product.model";
+import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
